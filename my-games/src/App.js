3058086a9f1491a83862e8a6a7941b513ecd62d2
fethy <fethy.amara@icloud.com>
2021-04-20 13:39:08 +0200

    header

--- my-games/src/App.js
+++ my-games/src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Game from './components/Game'
+import './components/Body'
+import './components/Square'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.basic = [
+      [0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0]
+    ]
+    this.state = {
+      tourJoueurJaune: true,
+      endGame: 0,
+      board: this.basic,
+      hoverIndex: null
+    }
+  }
+  componentDidUpdate() {
+    if (this.state.endGame === 1) {
+      this.resetBoard()
+      alert('Victoire des rouges');
+    } else if (this.state.endGame === 2) {
+      this.resetBoard()
+      alert('Victoire des jaunes');
+    }
+  }
+
+  resetBoard = () => {
+    const newBoard = [
+      [0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0]
+    ];
+    this.setState({ board: newBoard, endGame: 0 });
+  }
+  handleHover = (x) => {
+    this.setState({ hoverIndex: x })
+  }
+  handleMouseLeave = () => {
+    this.setState({ hoverIndex: null })
+  }
+
+  handleClick = x => {
+    console.log("x", x);
+    const joueur = this.state.tourJoueurJaune;
+    let myNewBoard = [...this.state.board];
+    const myCol = myNewBoard[x];
+  
+  }
 }
+
 
 export default App;
---
+++ my-games/src/components/Game.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+class Game extends React.Component {
+
+    render() {
+  
+      return (
+        <div className="App">
+  vhv
+        </div>
+      );
+    }
+  }
+  
+  export default Game;
